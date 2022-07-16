6ec59039ca9a72e48a24f3564727f2095260b952
Suvigya Agrawal <suvigyaagrawal411@gmail.com>
2022-07-16 21:30:37 +0530

    Connecting database

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const mongoose =require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost/blog',{               //line of codes to connect our database with the help of mongoose
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log("Database connected!"))
+.catch(err => console.log(err))
 
-app.use('/articles', articleRouter)
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     const articles = [{
@@ -17,7 +22,9 @@ app.get('/', (req, res) => {
         createdAt: (new Date()).toDateString() +' - '+ (new Date()).toLocaleTimeString().toUpperCase() + ' (India Standard Time)',
         description: 'This is second article of our blog'
     }]
-    res.render('index', {articles: articles})
+    res.render('articles/index', {articles: articles})
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
